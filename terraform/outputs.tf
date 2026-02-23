93485878e8f14fb2e23a6c4a651d0610921c97a3
FlorianBll <florian.belloc@limayrac.com>
2026-02-23 22:20:40 +0100

    fix output.tf misspeling values

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "ressource_group_id" {
     description = "ID du groupe"
-    value = azurerm_ressource.example.id
+    value = azurerm_ressource.rg.id
 }
 
 output "vnet_name" {
     description = "nom du reseau virtuel"
-    value = azurerm_virtual_network.example.name
+    value = azurerm_virtual_network.vnet.name
 }
 
 output "vnet_subnet_id" {
     description = "nom de sous-réseau du réseau virtuel"
-    value = azurerm_virtual_network.example.subnet.name
+    value = azurerm_virtual_network.vnet.subnet.name
 }
