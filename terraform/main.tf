86df3f4f50c1a72ab082d00bded8a6584de71631
FlorianBll <florian.belloc@limayrac.com>
2026-02-23 22:27:44 +0100

    fix location in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_public_ip" "lb_pip" {
   name                = "pip-loadbalancer"
-  location            = "azurerm_resource_group.rg.location"
+  location            = "zurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
